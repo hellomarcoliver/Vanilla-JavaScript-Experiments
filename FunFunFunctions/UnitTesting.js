a16156b872e4da0e220083aac7ccb9a5045f9dbd
Marc-Oliver Gern <mo.gern@gmail.com>
2017-11-19 16:16:54 +0000

    Change UnitTest JS

--- FunFunFunctions/UnitTesting.js
+++ FunFunFunctions/UnitTesting.js
@@ -1,3 +1,4 @@
+// items in a basket as an object with an array
 const someOrder = {
     items: [
         { name: 'Dragon Food', price: 80 },
@@ -5,9 +6,15 @@ const someOrder = {
     ]
 }
 
-const orderTotal = order => order.items.reduce((prev, cur) => prev + cur.price, 0)
+// calculate Total and add it to variable orderTotal
+const orderTotal = order => order.items
+    .reduce((prev, cur) => prev + cur.price, 0)
+
 
 result = orderTotal(someOrder)
 // result 880
 
 console.log(orderTotal)
+
+
+// REDUCE ////////////////////////////////////////////
